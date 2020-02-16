949ed4d6cb4aa22f54a1198ff1b6fdeef9abe17f
biningo <biningo@yeah.net>
2020-02-17 00:41:10 +0800

    完成时间轴日记模块以及分页

--- model/TimeLine.go
+++ model/TimeLine.go
@@ -1,5 +1,26 @@
 package model
 
+import "time"
+
 type TimeLine struct {
-	BaseModel
+	ID        int64     `form:"id" json:"id"`
+	CreatedAt time.Time `form:"created_at" json:"created_at"`
+	MkValue   string    `form:"mkValue" json:"mkValue" gorm:"type:text"`
+	UserName  string    `form:"username" json:"username"`
+}
+
+//不提供修改
+
+func (this TimeLine) GetAll(page int) (timeLines []TimeLine, total int64) {
+	db.Model(&TimeLine{}).Limit(10).Offset((page - 1) * 10).Order("created_at desc").Find(&timeLines)
+	db.Model(&TimeLine{}).Count(&total)
+	return
+}
+
+func (this *TimeLine) Add() {
+	db.Create(this)
+}
+
+func (this *TimeLine) Delete() {
+	db.Delete(this)
 }
